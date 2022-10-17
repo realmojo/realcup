f9368ce7f27691326fec28f22f8e99f03f22979b
realmojo88 <strikers1999@naver.com>
2022-10-17 17:33:16 +0900

    up

--- components/Adsense.js
+++ components/Adsense.js
@@ -1,32 +1,19 @@
-import React, { useEffect } from "react";
+import React from "react";
+import AdSense from "react-adsense";
 
-export const Adsense = ({ slotId, adFormat = "auto", height }) => {
+export const Adsense = ({ slotId, adFormat = "auto", height = 280 }) => {
   const isProduction = process.env.NODE_ENV === "production" ? true : false;
-  useEffect(() => {
-    try {
-      if (isProduction) {
-        (window.adsbygoogle = window.adsbygoogle || []).push({});
-      }
-    } catch (err) {
-      console.log(err);
-    }
-  }, []);
-
   return (
     <>
       {isProduction ? (
-        <div style={{ width: "100%", height: height ? `${height}px` : "none" }}>
-          <ins
-            className="adsbygoogle"
-            style={{
-              display: "block",
-              height: height ? `${height}px` : "none",
-            }}
-            data-ad-client="ca-pub-9130836798889522"
-            data-ad-slot={slotId}
-            data-ad-format={adFormat}
-            data-full-width-responsive="true"
-          ></ins>
+        <div style={{ width: "100%", height }}>
+          <AdSense.Google
+            style={{ display: "block", height }}
+            client="ca-pub-9130836798889522"
+            slot={slotId}
+            format={adFormat}
+            responsive="true"
+          />
         </div>
       ) : (
         <div style={{ width: "100%", height: "90px", backgroundColor: "gray" }}>
